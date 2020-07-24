bf443ff2a1f4c725340ffcb0a6710bbb6456d6d3
wenxiy <193367189@qq.com>
2020-07-24 19:45:56 +0800

    update

--- AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.asynctask;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.content.AsyncTaskLoader;
 
+import android.bluetooth.le.ScanSettings;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,49 +20,64 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView textView;
+    private DownloadFilesTask downloadFilesTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_name);
+        progressBar = findViewById(R.id.progress_bar);
+        downloadFilesTask = new DownloadFilesTask();
+        downloadFilesTask.execute();
     }
-    private class DownloadFilesTask extends AsyncTask<URL, Integer, Bitmap>{
-         URL url;
-         HttpURLConnection connection = null;
-         Bitmap bitmap;
-        @Override
-        protected Bitmap doInBackground(URL... urls) {//子线程中进行耗时任务
-            try {
-                url=urls[0];
-                connection=(HttpURLConnection)url.openConnection();
-                connection.setConnectTimeout(6000);
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                InputStream inputStream=connection.getInputStream();
-                bitmap= BitmapFactory.decodeStream(inputStream);
-                inputStream.close();
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+    private class DownloadFilesTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {//子线程中进行耗时任务
+            try {
+                int count = 0;
+                int length = 1;
+                while (count < 99) {
+                    count+=length;
+                    publishProgress(count);
+
+                }
+                Thread.sleep(50);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            return bitmap;
+            return null;
         }
 
         @Override
         protected void onPreExecute() {//最先执行，主线程中更新UI
+            textView.setText("正在加载");
             super.onPreExecute();
         }
 
         @Override
-        protected void onPostExecute(Bitmap aLong) {//返回结果到主线程
+        protected void onPostExecute(String aLong) {//返回结果到主线程
             super.onPostExecute(aLong);
-
+            textView.setText("加载完成");
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {//更新进度到主线程
             super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+            textView.setText("百分比"+values[0]+"%");
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            textView.setText("取消");
+            progressBar.setProgress(0);
+            super.onCancelled();
         }
     }
 }
