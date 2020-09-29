df2ecc9463a2f7dc063e5ca4a8c46d9565f0720a
wenxiy <193367189@qq.com>
2020-09-29 22:59:26 +0800

    update

---
+++ AsyncTask/app/src/main/java/com/example/asynctask/AsyncTask.java
@@ -0,0 +1,8 @@
+package com.example.asynctask;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class AsyncTask {
+    private ThreadPoolExecutor mthreadPoolExecutor;
+
+}
--- AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -1,23 +1,11 @@
 package com.example.asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
-
-import android.bluetooth.le.ScanSettings;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
@@ -33,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         downloadFilesTask = new DownloadFilesTask();
         downloadFilesTask.execute();
     }
-
     private class DownloadFilesTask extends AsyncTask<String, Integer, String> {
 
         @Override
@@ -43,10 +30,10 @@ public class MainActivity extends AppCompatActivity {
                 int length = 1;
                 while (count < 99) {
                     count+=length;
+                    Thread.sleep(400);
                     publishProgress(count);
 
                 }
-                Thread.sleep(50);
             } catch (Exception e) {
                 e.printStackTrace();
             }
